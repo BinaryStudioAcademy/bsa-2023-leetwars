65c6a9db1f13908eb364c5d1d0b92c9376f58543
Limbo2332 <vorobiovalosha@gmail.com>
2023-08-21 12:28:31 +0300

    added implementation of canactivatechild

--- frontend/src/app/core/guards/auth.guard.ts
+++ frontend/src/app/core/guards/auth.guard.ts
@@ -1,16 +1,24 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { CanActivate, CanActivateChild, Router } from '@angular/router';
 import { AuthService } from '@core/services/auth.service';
 
 @Injectable({
     providedIn: 'root',
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanActivateChild {
     constructor(private router: Router, private authService: AuthService) {
         // do nothing
     }
 
     public canActivate() {
+        return this.checkActivation();
+    }
+
+    public canActivateChild() {
+        return this.checkActivation();
+    }
+
+    private checkActivation() {
         if (this.authService.isAuthorized()) {
             return true;
         }
