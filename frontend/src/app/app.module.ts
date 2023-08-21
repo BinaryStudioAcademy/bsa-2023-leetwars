a6ee74b58e68ef38a1efd28ca23fa7c37a7fea28
pospyra <pos.pyra@yandex.ru>
2023-08-21 11:12:53 +0300

    implemented snack-bar-service

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from '@shared/shared.module';
 
 import { AppComponent } from './app.component';
@@ -7,7 +9,7 @@ import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, SharedModule, AppRoutingModule],
+    imports: [BrowserModule, SharedModule, AppRoutingModule, MatSnackBarModule, BrowserAnimationsModule],
     providers: [],
     bootstrap: [AppComponent],
 })
