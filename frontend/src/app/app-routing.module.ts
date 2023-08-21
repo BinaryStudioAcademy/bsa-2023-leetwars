2bd5a7762bbd1b8d960deb31d579f12e7b4acdf7
pospyra <pos.pyra@yandex.ru>
2023-08-21 19:45:59 +0300

    added not-found page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from '@shared/components/not-found/not-found.component';
 
 const routes: Routes = [
     {
@@ -10,7 +11,8 @@ const routes: Routes = [
         path: 'main',
         loadChildren: () => import('./modules/main/main.module').then((m) => m.MainModule),
     },
-    { path: '**', redirectTo: '', pathMatch: 'full' },
+    { path: '**', redirectTo: '/404', pathMatch: 'full' },
+    { path: '404', component: NotFoundComponent, pathMatch: 'full' },
 ];
 
 @NgModule({
