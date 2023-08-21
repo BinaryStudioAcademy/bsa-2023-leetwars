4dd176d690df642d0c877ff72c56c0e4c005677c
pospyra <pos.pyra@yandex.ru>
2023-08-21 16:13:40 +0300

    fixed code format

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,22 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SharedModule } from '@shared/shared.module';
 import { ToastrModule } from 'ngx-toastr';
 
+import { SharedModule } from './shared/shared.module';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [
-        BrowserModule,
-        SharedModule,
-        AppRoutingModule,
-        BrowserAnimationsModule,
-        ToastrModule.forRoot(),
-    ],
     providers: [],
     bootstrap: [AppComponent],
+    imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, ToastrModule.forRoot(), SharedModule],
 })
 export class AppModule {}
