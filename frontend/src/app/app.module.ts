c20d711566a204d38006382b19b573f5ec1696b5
pospyra <pos.pyra@yandex.ru>
2023-08-21 15:33:54 +0300

    changed ui library to ngx

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from '@shared/shared.module';
+import { ToastrModule } from 'ngx-toastr';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, SharedModule, AppRoutingModule, MatSnackBarModule, BrowserAnimationsModule],
+    imports: [
+        BrowserModule,
+        SharedModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        ToastrModule.forRoot(),
+    ],
     providers: [],
     bootstrap: [AppComponent],
 })
---
+++ frontend/src/app/core/services/toastr-notifications.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class ToastrNotificationsService {
+    // eslint-disable-next-line no-empty-function
+    constructor(private toastr: ToastrService) {}
+
+    showSuccess(message: string) {
+        this.toastr.success(message, 'Success');
+    }
+
+    showInfo(message: string) {
+        this.toastr.info(message, 'Info');
+    }
+
+    showWarring(message: string) {
+        this.toastr.warning(message, 'Warring');
+    }
+
+    showError(message: string) {
+        this.toastr.error(message, 'Error');
+    }
+}
