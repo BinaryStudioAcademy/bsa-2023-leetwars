0fe558ffb939b367a57358c988f8742aedc8a5c6
Limbo2332 <vorobiovalosha@gmail.com>
2023-08-21 13:39:08 +0300

    added fontawesome library

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -2,12 +2,16 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
 
 import { LoadingSpinnerComponent } from './components/loading-spinner/loading-spinner.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
-    imports: [CommonModule, RouterModule, FormsModule, ReactiveFormsModule, RouterModule],
+    imports: [CommonModule, RouterModule, FormsModule, ReactiveFormsModule, RouterModule, FontAwesomeModule],
     declarations: [LoadingSpinnerComponent, NotFoundComponent],
     exports: [
         CommonModule,
@@ -15,8 +19,13 @@ import { NotFoundComponent } from './components/not-found/not-found.component';
         FormsModule,
         ReactiveFormsModule,
         RouterModule,
+        FontAwesomeModule,
         LoadingSpinnerComponent,
         NotFoundComponent,
     ],
 })
-export class SharedModule {}
+export class SharedModule {
+    constructor(library: FaIconLibrary) {
+        library.addIconPacks(fas, fab, far);
+    }
+}
