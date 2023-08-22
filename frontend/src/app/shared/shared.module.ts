623dd71ba6383058ada604d0b8d9855789cefbac
Limbo2332 <vorobiovalosha@gmail.com>
2023-08-22 21:44:48 +0300

    added temporary template for main page

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -6,15 +6,27 @@ import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontaweso
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import { far } from '@fortawesome/free-regular-svg-icons';
 import { fas } from '@fortawesome/free-solid-svg-icons';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
+import { ChallengeComponent } from './components/challenge/challenge.component';
 import { ConfirmationModalComponent } from './components/confirmation-modal/confirmation-modal.component';
+import { DropdownSelectComponent } from './components/dropdown-select/dropdown-select.component';
+import { FilteringSectionComponent } from './components/filtering-section/filtering-section.component';
 import { GeneralModalComponent } from './components/general-modal/general-modal.component';
 import { LoadingSpinnerComponent } from './components/loading-spinner/loading-spinner.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
-    imports: [CommonModule, RouterModule, FormsModule, ReactiveFormsModule, RouterModule, FontAwesomeModule],
-    declarations: [LoadingSpinnerComponent, NotFoundComponent, ConfirmationModalComponent, GeneralModalComponent],
+    imports: [CommonModule, RouterModule, FormsModule, ReactiveFormsModule, RouterModule, FontAwesomeModule, NgbModule],
+    declarations: [
+        LoadingSpinnerComponent,
+        NotFoundComponent,
+        ConfirmationModalComponent,
+        GeneralModalComponent,
+        DropdownSelectComponent,
+        ChallengeComponent,
+        FilteringSectionComponent,
+    ],
     exports: [
         CommonModule,
         RouterModule,
@@ -27,6 +39,8 @@ import { NotFoundComponent } from './components/not-found/not-found.component';
         ConfirmationModalComponent,
         GeneralModalComponent,
         DropdownSelectComponent,
+        ChallengeComponent,
+        FilteringSectionComponent,
     ],
 })
 export class SharedModule {
