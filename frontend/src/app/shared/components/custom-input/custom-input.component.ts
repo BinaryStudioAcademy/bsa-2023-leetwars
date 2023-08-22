da173a7b213627cdb3cd49717975cf0fef8ec168
Bohdan <bodichka322@gmail.com>
2023-08-23 00:34:31 +0300

    Add new variables and bindings

--- frontend/src/app/shared/components/custom-input/custom-input.component.ts
+++ frontend/src/app/shared/components/custom-input/custom-input.component.ts
@@ -1,7 +1,7 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
-    selector: 'app-custom-input',
+    selector: 'app-custom-input[Identifier]',
     templateUrl: './custom-input.component.html',
     styleUrls: ['./custom-input.component.sass'],
 })
@@ -10,19 +10,16 @@ export class CustomInputComponent {
 
     @Input() InputType: string = 'text';
     @Input() Identifier: string = 'text';
-    @Input() Height: string;
-    @Input() Width: string;
+    @Input() Height: string = '64px';
+    @Input() Width: string = '327px';
+    @Input() FormTextHeight: string = '45px';
     @Input() InputValue?: string;
     @Input() InputLabel?: string;
     @Input() InputPlaceholder?: string;
     @Output() InputValueChange = new EventEmitter<string>();
 
-  onInputChange(model: string)
-  {
-    this.InputValue = model;
-    this.InputValueChange.emit(model);
-  }
-    
-
-
+    onInputChange(model: string) {
+        this.InputValue = model;
+        this.InputValueChange.emit(model);
+    }
 }
