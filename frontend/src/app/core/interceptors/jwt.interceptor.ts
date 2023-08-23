58236cdbfc087e9d736795518f26543e0061cb51
Stanislav-Ostr <stanislav.ostr@gmail.com>
2023-08-23 14:35:00 +0300

    Frontend: Merged firebase service in auth service

--- frontend/src/app/core/interceptors/jwt.interceptor.ts
+++ frontend/src/app/core/interceptors/jwt.interceptor.ts
@@ -4,12 +4,15 @@ import {
     HttpInterceptor,
     HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { AuthService } from '@core/services/auth.service';
 import { Observable } from 'rxjs';
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
+    constructor(private firebaseAuthService: AuthService) {}
+
     intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-        const userToken = localStorage.getItem('user-token');
+        const userToken = this.firebaseAuthService.getUserToken();
 
         if (!userToken) {
             return next.handle(request);
