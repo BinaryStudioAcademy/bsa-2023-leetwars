f4c71844f3bae968f275acf3c3a8596f83e8ca8d
AyTee001 <tiutiunanastasia@gmail.com>
2023-08-23 17:23:58 +0300

    Add monaco editor packages

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { SharedModule } from '@shared/shared.module';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { MonacoEditorModule } from 'ngx-monaco-editor-v2';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, SharedModule, AppRoutingModule],
+    imports: [BrowserModule, SharedModule, AppRoutingModule, MonacoEditorModule.forRoot()],
     providers: [],
     bootstrap: [AppComponent],
 })
