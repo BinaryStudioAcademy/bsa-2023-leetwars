3f2001f68d5a9a6ddd2d39dc3bb658772e88cac0
Максим Шимків <322topgamerintheworld228@gmail.com>
2023-08-23 17:48:50 +0300

    Add Suggested Challenge component UI

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -13,6 +13,7 @@ import { DropdownSelectComponent } from './components/dropdown-select/dropdown-s
 import { GeneralModalComponent } from './components/general-modal/general-modal.component';
 import { LoadingSpinnerComponent } from './components/loading-spinner/loading-spinner.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { SuggestedChallengeComponent } from './components/suggested-challenge/suggested-challenge.component';
 
 @NgModule({
     imports: [CommonModule, RouterModule, FormsModule, ReactiveFormsModule, RouterModule, FontAwesomeModule, NgbModule],
@@ -22,6 +23,7 @@ import { NotFoundComponent } from './components/not-found/not-found.component';
         ConfirmationModalComponent,
         GeneralModalComponent,
         DropdownSelectComponent,
+        SuggestedChallengeComponent,
     ],
     exports: [
         CommonModule,
@@ -35,6 +37,7 @@ import { NotFoundComponent } from './components/not-found/not-found.component';
         ConfirmationModalComponent,
         GeneralModalComponent,
         DropdownSelectComponent,
+        SuggestedChallengeComponent,
     ],
 })
 export class SharedModule {
