0f2de1fbb3bd0e69098e510f1eaf0e0b4e39a2f0
pospyra <pos.pyra@yandex.ru>
2023-08-23 13:34:36 +0300

    resolved fit conflict

--- backend/LeetWars.Core/LeetWars.Core.Common/DTO/Language/LanguageDto.cs
+++ backend/LeetWars.Core/LeetWars.Core.Common/DTO/Language/LanguageDto.cs
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LeetWars.Core.Common.DTO.Language
-{
-    public class LanguageDto
-    {
-        public int Id { get; set; }
-
-        public string Name { get; set; }
-=======
 ﻿using LeetWars.Core.DAL.Entities;
 
 namespace LeetWars.Core.Common.DTO.Language
@@ -20,6 +5,5 @@ namespace LeetWars.Core.Common.DTO.Language
     public class LanguageDto : Entity<int>
     {
         public string Name { get; set; } = string.Empty;
->>>>>>> feature/11-implement-entities
     }
 }
--- backend/LeetWars.Core/LeetWars.Core.Common/DTO/Tag/TagDto.cs
+++ backend/LeetWars.Core/LeetWars.Core.Common/DTO/Tag/TagDto.cs
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LeetWars.Core.Common.DTO.Tag
-{
-    public  class TagDto
-    {
-        public int Id { get; set; }
-
-        public string Name { get; set; }
-=======
 ﻿using LeetWars.Core.DAL.Entities;
 
 namespace LeetWars.Core.Common.DTO.Tag
@@ -20,6 +5,5 @@ namespace LeetWars.Core.Common.DTO.Tag
     public class TagDto : Entity<int>
     {
         public string Name { get; set; } = string.Empty;    
->>>>>>> feature/11-implement-entities
     }
 }
