f551f2576e8fe81fb3183e9e4dbe962a7656b0d6
Stanislav-Ostr <stanislav.ostr@gmail.com>
2023-08-23 10:43:34 +0300

    JWT interceptor: Get user token from local storage

--- frontend/src/app/core/interceptors/jwt.interceptor.ts
+++ frontend/src/app/core/interceptors/jwt.interceptor.ts
@@ -4,15 +4,13 @@ import {
     HttpInterceptor,
     HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { FirebaseAuthService } from '@core/services/firebase-auth.service';
 import { Observable } from 'rxjs';
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
-    constructor(private firebaseAuthService: FirebaseAuthService) {}
 
     intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-        const userToken = this.firebaseAuthService.getUserToken();
+        const userToken = localStorage.getItem('user-token');
 
         if (!userToken) {
             return next.handle(request);
