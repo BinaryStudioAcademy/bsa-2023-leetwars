90b9c25e5bfe95944ac425b52b9801e0c5a52211
AyTee001 <tiutiunanastasia@gmail.com>
2023-08-23 18:38:39 +0300

    Added base for code editor

---
+++ frontend/src/app/shared/components/code-editor/code-editor.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input } from '@angular/core';
+
+interface EditorOptions{
+  language: string,
+}
+
+@Component({
+  selector: 'app-code-editor[language]',
+  templateUrl: './code-editor.component.html',
+  styleUrls: ['./code-editor.component.sass']
+})
+export class CodeEditorComponent {
+
+  @Input() language: string;
+  @Input() initSolution: string = '';
+
+  editorOptions: EditorOptions;
+
+  constructor() 
+  { 
+    this.editorOptions = {
+      language: this.language,
+    }
+  }
+}
--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -2,13 +2,15 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { MonacoEditorModule } from 'ngx-monaco-editor-v2';
 
 import { LoadingSpinnerComponent } from './components/loading-spinner/loading-spinner.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { CodeEditorComponent } from './components/code-editor/code-editor.component';
 
 @NgModule({
-    imports: [CommonModule, RouterModule, FormsModule, ReactiveFormsModule, RouterModule],
-    declarations: [LoadingSpinnerComponent, NotFoundComponent],
+    imports: [CommonModule, RouterModule, FormsModule, ReactiveFormsModule, RouterModule, MonacoEditorModule],
+    declarations: [LoadingSpinnerComponent, NotFoundComponent, CodeEditorComponent],
     exports: [
         CommonModule,
         RouterModule,
@@ -17,6 +19,7 @@ import { NotFoundComponent } from './components/not-found/not-found.component';
         RouterModule,
         LoadingSpinnerComponent,
         NotFoundComponent,
+        CodeEditorComponent
     ],
 })
 export class SharedModule {}
