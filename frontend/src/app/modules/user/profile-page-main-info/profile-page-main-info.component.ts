4cc94da215e14a171e61a467b7e69c59ece470b7
SashaBondarchuk <o.bondarchuk9962@gmail.com>
2023-08-23 19:28:48 +0300

    added profile main page component files

---
+++ frontend/src/app/modules/user/profile-page-main-info/profile-page-main-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-page-main-info',
+  templateUrl: './profile-page-main-info.component.html',
+  styleUrls: ['./profile-page-main-info.component.sass']
+})
+export class ProfilePageMainInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
