b7ca26b1dfafd3cbcdfd822268b1cc81409d28c4
pospyra <pos.pyra@yandex.ru>
2023-08-24 18:03:27 +0300

    added id to challengePreviewDto

--- backend/LeetWars.Core/LeetWars.Core.Common/DTO/Challenge/ChallengePreviewDto.cs
+++ backend/LeetWars.Core/LeetWars.Core.Common/DTO/Challenge/ChallengePreviewDto.cs
@@ -1,5 +1,6 @@
 ﻿using LeetWars.Core.Common.DTO.Language;
 using LeetWars.Core.Common.DTO.Tag;
+using LeetWars.Core.DAL.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace LeetWars.Core.Common.DTO.Challenge
 {
-    public class ChallengePreviewDto
+    public class ChallengePreviewDto : Entity<long>
     {
         public string AuthorName { get; set; } = string.Empty;
 
