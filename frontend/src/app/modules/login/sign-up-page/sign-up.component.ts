983bd7e0b5cdb9db4682e89e95ac183a2917e7dd
Limbo2332 <vorobiovalosha@gmail.com>
2023-08-24 15:45:08 +0300

    refactored custominput ngmodel and finished with signup page

--- frontend/src/app/modules/login/sign-up-page/sign-up.component.ts
+++ frontend/src/app/modules/login/sign-up-page/sign-up.component.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-sign-up',
     templateUrl: './sign-up.component.html',
     styleUrls: ['./sign-up.component.sass'],
 })
-export class SignUpComponent {}
+export class SignUpComponent implements OnInit {
+    public registrationForm: FormGroup;
+
+    constructor(private fb: FormBuilder) {}
+
+    public ngOnInit() {
+        //TODO: Add validations to fields
+        this.registrationForm = this.fb.group({
+            email: ['', [Validators.required, Validators.email]],
+            username: ['', [Validators.required]],
+            password: ['', [Validators.required]],
+        });
+    }
+}
