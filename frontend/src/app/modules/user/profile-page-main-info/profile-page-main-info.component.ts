ae01e92191543e51e329f14c5bbd20321f0bad93
SashaBondarchuk <o.bondarchuk9962@gmail.com>
2023-08-25 12:01:25 +0300

    added data binding

--- frontend/src/app/modules/user/profile-page-main-info/profile-page-main-info.component.ts
+++ frontend/src/app/modules/user/profile-page-main-info/profile-page-main-info.component.ts
@@ -5,4 +5,25 @@ import { Component } from '@angular/core';
     templateUrl: './profile-page-main-info.component.html',
     styleUrls: ['./profile-page-main-info.component.sass'],
 })
-export class ProfilePageMainInfoComponent {}
+export class ProfilePageMainInfoComponent {
+    //replace it with real user interface
+    public username = 'TatiHutii';
+
+    public userRank = '~5,000,000';
+
+    public communityViews = 0;
+
+    public communityLastWeekViews = 0;
+
+    public communitySolution = 0;
+
+    public communityLastWeekSolution = 0;
+
+    public communityDiscuss = 0;
+
+    public communityLastWeekDiscuss = 0;
+
+    public communityReputation = 0;
+
+    public communityLastWeekReputation = 0;
+}
