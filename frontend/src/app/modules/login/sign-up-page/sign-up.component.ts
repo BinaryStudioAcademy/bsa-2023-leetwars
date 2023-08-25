ba1bdfebf88c6515c24d339b5c89a7c8535427a3
Lintur Vladyslav <88484289+Illusion4@users.noreply.github.com>
2023-08-25 10:38:30 +0300

    Delete unused method

--- frontend/src/app/modules/login/sign-up-page/sign-up.component.ts
+++ frontend/src/app/modules/login/sign-up-page/sign-up.component.ts
@@ -30,8 +30,4 @@ export class SignUpComponent {
                 this.router.navigateByUrl('');
             });
     }
-
-    public onSignInWithGitHub() {
-        console.log(this.signUpForm);
-    }
 }
