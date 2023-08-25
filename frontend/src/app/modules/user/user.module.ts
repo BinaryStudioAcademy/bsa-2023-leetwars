c33ff1fe4090eb2dec1888b97397c818bd87053a
Stanislav-Ostr <stanislav.ostr@gmail.com>
2023-08-25 14:09:47 +0300

    Add component declaration

--- frontend/src/app/modules/user/user.module.ts
+++ frontend/src/app/modules/user/user.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
+import { SubmissionsChartComponent } from '@modules/user/submissions-chart/submissions-chart.component';
 import { UserRoutingModule } from '@modules/user/user-routing.module';
 import { SharedModule } from '@shared/shared.module';
 
 @NgModule({
-    declarations: [],
+    declarations: [SubmissionsChartComponent],
     imports: [SharedModule, UserRoutingModule],
 })
 export class UserModule { }
