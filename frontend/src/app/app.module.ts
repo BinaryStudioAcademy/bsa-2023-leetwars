084c381eb5685bb613797cb5f46ab26c2effb6d6
Illusion4 <vladyslavlintur@gmail.com>
2023-08-25 13:28:06 +0300

    Add HTTP module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFireAuthModule } from '@angular/fire/compat/auth';
@@ -24,6 +25,7 @@ import { AppRoutingModule } from './app-routing.module';
         SharedModule,
         AngularFireModule.initializeApp(environment.firebaseConfig, 'leetwars'),
         AngularFireAuthModule,
+        HttpClientModule,
     ],
 })
 export class AppModule {}
