79a53ab30f6a55d82ff03a0eed2af53d60e96e70
SashaBondarchuk <o.bondarchuk9962@gmail.com>
2023-08-26 22:00:03 +0300

    added consumer service

---
+++ backend/LeetWars.Notifier/LeetWars.Notifier.WebAPI/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Options;
+using RabbitMQ.Client;
+
+namespace LeetWars.RabbitMQ.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void RegisterCustomServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<ConsumerSettings>(configuration.GetSection("RabbitMQConsumer"));
+            services.AddSingleton(sp =>
+            {
+                var rabbitUri = new Uri(configuration["Rabbit"]);
+                var factory = new ConnectionFactory { Uri = rabbitUri };
+                return factory.CreateConnection();
+            });
+            services.AddSingleton(sp => sp.GetRequiredService<IOptions<ConsumerSettings>>().Value);
+            services.AddTransient<IConsumerService, ConsumerService>();
+        }
+    }
+}
---
+++ backend/LeetWars.Notifier/LeetWars.Notifier.WebAPI/Services/MessageConsumerService.cs
@@ -0,0 +1,32 @@
+﻿using LeetWars.RabbitMQ;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace LeetWars.Notifier.WebAPI.Services
+{
+    public class MessageConsumerService : BackgroundService
+    {
+        private readonly IServiceScopeFactory _serviceScopeFactory;
+
+        public MessageConsumerService(IServiceScopeFactory serviceScopeFactory)
+        {
+            _serviceScopeFactory = serviceScopeFactory;
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            using var scope = _serviceScopeFactory.CreateScope();
+            var consumerService = scope.ServiceProvider.GetRequiredService<IConsumerService>();
+            var handler = new EventHandler<BasicDeliverEventArgs>((model, args) =>
+            {
+                var body = args.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(message); //impement some logic here using signalR
+                consumerService.SetAcknowledge(args.DeliveryTag, true);
+            });
+
+            consumerService.Listen(handler);
+            return Task.CompletedTask;
+        }
+    }
+}
