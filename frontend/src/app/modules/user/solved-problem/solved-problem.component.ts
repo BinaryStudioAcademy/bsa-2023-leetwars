239b98ea615885d89d6118ba62ba6a5380f0ff79
Богдан Дяків <bodichka322@gmail.com>
2023-08-26 13:55:58 +0300

    Fix pie chart

--- frontend/src/app/modules/user/solved-problem/solved-problem.component.ts
+++ frontend/src/app/modules/user/solved-problem/solved-problem.component.ts
@@ -1,21 +1,29 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
     selector: 'app-solved-problem',
     templateUrl: './solved-problem.component.html',
     styleUrls: ['./solved-problem.component.sass'],
 })
-export class SolvedProblemComponent implements OnInit {
+export class SolvedProblemComponent {
     constructor() {}
+
     @Input() color: string;
+
     @Input() easyTasksCompleted: number = 100;
+
     @Input() mediumTasksCompleted: number = 250;
+
     @Input() hardTasksCompleted: number = 550;
+
     @Input() easyTasks: number = 300;
+
     @Input() mediumTasks: number = 500;
+
     @Input() hardTasks: number = 700;
-    @Input() totalTasks: number = (this.easyTasks + this.mediumTasks + this.hardTasks);
-    @Input() totalTasksCompleted: number = (this.easyTasksCompleted + this.mediumTasksCompleted + this.hardTasksCompleted);
-    @Input() solved: number =(this.totalTasks-this.totalTasksCompleted);
-    ngOnInit(): void {}
+
+    @Input() totalTasks: number = this.easyTasks + this.mediumTasks + this.hardTasks;
+
+    @Input() totalTasksCompleted: number =
+        this.easyTasksCompleted + this.mediumTasksCompleted + this.hardTasksCompleted;
 }
--- frontend/src/app/shared/components/pie-chart/pie-chart.component.ts
+++ frontend/src/app/shared/components/pie-chart/pie-chart.component.ts
@@ -2,48 +2,41 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Color, ScaleType } from '@swimlane/ngx-charts';
 
 @Component({
-  selector: 'app-pie-chart',
-  templateUrl: './pie-chart.component.html',
-  styleUrls: ['./pie-chart.component.sass']
+    selector: 'app-pie-chart',
+    templateUrl: './pie-chart.component.html',
+    styleUrls: ['./pie-chart.component.sass'],
 })
 export class PieChartComponent implements OnInit {
-  solved: number = 0; 
-  @Input() CurrentNumber: number;
-  @Input() TotalNumber: number;
-  @Input() activeColor: string;
- 
-  
+    @Input() CurrentNumber: number;
+    @Input() TotalNumber: number;
+    @Input() activeColor: string;
 
-  single: any[] = [];
-  colorScheme!: Color;
-  
+    single: any[] = [];
+    colorScheme!: Color;
 
-  view: [number, number] = [140, 140]; //height & width
+    view: [number, number] = [140, 140]; //height & width
 
-  constructor(
-  ) {
+    constructor() {
+        Object.assign(this, { this: this.single });
+    }
 
-    Object.assign(this, { this: this.single });
-  }
+    ngOnInit(): void {
+        this.single = [
+            {
+                name: 'Solved',
+                value: this.CurrentNumber,
+            },
+            {
+                name: 'Rest',
+                value: this.TotalNumber - this.CurrentNumber,
+            },
+        ];
 
-  ngOnInit(): void {
-    this.solved = Math.floor(this.TotalNumber-this.CurrentNumber);
-    this.single = [
-      {
-        "name": "Solved",
-        "value": this.TotalNumber-this.CurrentNumber, 
-      },
-      {
-        "name": "Total",
-        "value": this.TotalNumber,
-      },
-    ];
-
-    this.colorScheme = {
-      name: 'myScheme',
-      selectable: true,
-      group: ScaleType.Ordinal,
-      domain: [this.activeColor, '#F0F0F0']
-    };
-  }
+        this.colorScheme = {
+            name: 'myScheme',
+            selectable: true,
+            group: ScaleType.Ordinal,
+            domain: [this.activeColor, '#F0F0F0'],
+        };
+    }
 }
