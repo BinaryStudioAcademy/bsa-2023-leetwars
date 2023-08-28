18c3fab3bec745cae2d8a0c0451b55a237af1808
pospyra <pos.pyra@yandex.ru>
2023-08-28 18:38:10 +0300

    configurated validation for custom-input

--- frontend/src/app/shared/components/custom-input/custom-input.component.ts
+++ frontend/src/app/shared/components/custom-input/custom-input.component.ts
@@ -1,11 +1,52 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+/* eslint-disable no-empty-function */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable no-use-before-define */
+import { Component, EventEmitter, forwardRef, Input, Output } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
     selector: 'app-custom-input[Identifier]',
     templateUrl: './custom-input.component.html',
     styleUrls: ['./custom-input.component.sass'],
+    providers: [
+        {
+            provide: NG_VALUE_ACCESSOR,
+            multi: true,
+            useExisting: forwardRef(() => CustomInputComponent),
+        },
+    ],
 })
-export class CustomInputComponent {
+export class CustomInputComponent implements ControlValueAccessor {
+    onChange: any = () => {};
+
+    onTouchedFn = () => {};
+
+    private _value = '';
+
+    get value() {
+        return this._value;
+    }
+
+    set value(v) {
+        this._value = v;
+        if (this.onChange) {
+            this.onChange(this._value);
+        }
+    }
+
+    registerOnChange(fn: any): void {
+        this.onChange = fn;
+        this.onChange(this._value);
+    }
+
+    registerOnTouched(fn: any): void {
+        this.onTouchedFn = fn;
+    }
+
+    writeValue(value: any): void {
+        this.value = value;
+    }
+
     @Input() InputType: string = 'text';
 
     @Input() Identifier: string = 'text';
@@ -24,14 +65,14 @@ export class CustomInputComponent {
 
     @Output() InputValueChange = new EventEmitter<string>();
 
-    public showPassword = false;
+    showPassword = false;
 
-    public onInputChange(model: string) {
+    onInputChange(model: string) {
         this.InputValue = model;
         this.InputValueChange.emit(model);
     }
 
-    public togglePasswordVisibility() {
+    togglePasswordVisibility() {
         this.InputType = this.InputType === 'password' ? 'text' : 'password';
         this.showPassword = !this.showPassword;
     }
