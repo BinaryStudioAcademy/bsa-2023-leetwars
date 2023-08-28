b49094e12579896844f111fc810cc234d5e4618f
pashamed <pashamedenetes@gmail.com>
2023-08-28 16:54:45 +0300

    add mapping, add userService,

---
+++ backend/LeetWars.Core/LeetWars.Core.BLL/MappingProfiles/UserProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using LeetWars.Core.Common.DTO.User;
+using LeetWars.Core.DAL.Entities;
+
+namespace LeetWars.Core.BLL.MappingProfiles
+{
+    public sealed class UserProfile:Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<User,UserDto>();
+            CreateMap<UserDto,User>();
+            CreateMap<NewUserDto, User>();
+        }
+    }
+}
---
+++ backend/LeetWars.Core/LeetWars.Core.BLL/Services/UserService.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using LeetWars.Core.BLL.Exceptions;
+using LeetWars.Core.Common.DTO.User;
+using LeetWars.Core.DAL.Context;
+using LeetWars.Core.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace LeetWars.Core.BLL.Services
+{
+    internal class UserService : BaseService
+    {
+        public UserService(LeetWarsCoreContext context, IMapper mapper) : base(context, mapper)
+        {
+        }
+
+        public async Task<ICollection<UserDto>> GetUsers()
+        {
+            var users = await _context.Users.ToListAsync();
+
+            return _mapper.Map<ICollection<UserDto>>(users);
+        }
+
+        public async Task<UserDto> GetUserById(int id)
+        {
+            var user = await GetUserByIdInternal(id);
+            if (user == null)
+            {
+                throw new NotFoundException(nameof(User), id);
+            }
+
+            return _mapper.Map<UserDto>(user);
+        }
+
+
+        private async Task<User> GetUserByIdInternal(int id)
+        {
+            return await _context.Users
+                .FirstOrDefaultAsync(u => u.Id == id);
+        }
+    }
+}
