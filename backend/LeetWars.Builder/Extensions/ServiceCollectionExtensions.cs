688184493edc45c74096b108b23bb319aa5db1c2
Limbo2332 <vorobiovalosha@gmail.com>
2023-08-29 17:53:12 +0300

    fixed problem with running builder

--- backend/LeetWars.Builder/Extensions/ServiceCollectionExtensions.cs
+++ backend/LeetWars.Builder/Extensions/ServiceCollectionExtensions.cs
@@ -8,16 +8,16 @@ namespace LeetWars.Builder.Extensions
     {
         public static void RegisterProduceMessagesServices(this IServiceCollection services, IConfiguration configuration)
         {
+            services.Configure<ConsumerSettings>(configuration.GetSection("RabbitMQConsumer"));
+
             services.AddSingleton(con => new ConnectionFactory()
             {
                 Uri = new Uri(configuration["RabbitUri"])
             }
             .CreateConnection());
-
-            services.Configure<ConsumerSettings>(configuration.GetSection("RabbitMQConsumer"))
-                .AddSingleton<IConsumerService, ConsumerService>();
-
+                
             services.AddSingleton(sp => sp.GetRequiredService<IOptions<ConsumerSettings>>().Value);
+            services.AddSingleton<IConsumerService, ConsumerService>();
         }
 
     }
