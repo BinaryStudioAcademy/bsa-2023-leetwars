974b34496a021df7ed61b40501c736c7af9d3752
Stanislav-Ostr <stanislav.ostr@gmail.com>
2023-08-30 17:47:10 +0300

    Fix sonar warning#2

--- backend/LeetWars.Core/LeetWars.Core.BLL/Services/ChallengeService.cs
+++ backend/LeetWars.Core/LeetWars.Core.BLL/Services/ChallengeService.cs
@@ -1,4 +1,5 @@
-﻿using AutoMapper;
+﻿using System.Security.Cryptography;
+using AutoMapper;
 using LeetWars.Core.BLL.Interfaces;
 using LeetWars.Core.Common.DTO.Challenge;
 using LeetWars.Core.Common.DTO.Filters;
@@ -21,20 +22,21 @@ namespace LeetWars.Core.BLL.Services
             _userIdGetter = userIdGetter;
         }
 
-        public async Task<ICollection<ChallengePreviewDto>> GetChallengesAsync(ChallengesFiltersDto filters, PageSettingsDto? page)
+        public async Task<ICollection<ChallengePreviewDto>> GetChallengesAsync(ChallengesFiltersDto filters,
+            PageSettingsDto? page)
         {
             var userId = _userIdGetter.CurrentUserId;
-            
+
             var challenges = _context.Challenges
-                    .Include(challenge => challenge.Tags)
-                    .Include(challenge => challenge.Author)
-                    .Include(challenge => challenge.Level)
-                    .Include(challenge => challenge.Versions)
-                        .ThenInclude(version => version.Language)
-                    .Include(challenge => challenge.Versions)
-                        .ThenInclude(version => version.Solutions)
-                            .ThenInclude(solution => solution.User)
-                    .AsQueryable();
+                .Include(challenge => challenge.Tags)
+                .Include(challenge => challenge.Author)
+                .Include(challenge => challenge.Level)
+                .Include(challenge => challenge.Versions)
+                .ThenInclude(version => version.Language)
+                .Include(challenge => challenge.Versions)
+                .ThenInclude(version => version.Solutions)
+                .ThenInclude(solution => solution.User)
+                .AsQueryable();
 
             if (!string.IsNullOrEmpty(filters.Title))
             {
@@ -44,7 +46,7 @@ namespace LeetWars.Core.BLL.Services
 
             if (filters.ChallengeStatus.HasValue)
             {
-                challenges = challenges.Where(challenge => 
+                challenges = challenges.Where(challenge =>
                     challenge.Versions.Any(version => version.Status == filters.ChallengeStatus));
             }
 
@@ -58,9 +60,16 @@ namespace LeetWars.Core.BLL.Services
             {
                 challenges = filters.Progress switch
                 {
-                    ChallengesProgress.NotStarted => challenges.Where(challenge => challenge.Versions.All(version => version.Solutions.Count == 0 || version.Solutions.All(solution => solution.User == null || solution.User.Uid != userId))),
-                    ChallengesProgress.Started => challenges.Where(challenge => challenge.Versions.Any(version => version.Solutions.Any(solution => solution.User != null && solution.User.Uid == userId && solution.SubmittedAt == null))),
-                    ChallengesProgress.Completed => challenges.Where(challenge => challenge.Versions.Any(version => version.Solutions.Any(solution => solution.User != null && solution.User.Uid == userId && solution.SubmittedAt.HasValue && solution.SubmittedAt.Value != DateTime.MinValue))),
+                    ChallengesProgress.NotStarted => challenges.Where(challenge => challenge.Versions.All(version =>
+                        version.Solutions.Count == 0 || version.Solutions.All(solution =>
+                            solution.User == null || solution.User.Uid != userId))),
+                    ChallengesProgress.Started => challenges.Where(challenge => challenge.Versions.Any(version =>
+                        version.Solutions.Any(solution =>
+                            solution.User != null && solution.User.Uid == userId && solution.SubmittedAt == null))),
+                    ChallengesProgress.Completed => challenges.Where(challenge => challenge.Versions.Any(version =>
+                        version.Solutions.Any(solution =>
+                            solution.User != null && solution.User.Uid == userId && solution.SubmittedAt.HasValue &&
+                            solution.SubmittedAt.Value != DateTime.MinValue))),
                     _ => challenges
                 };
             }
@@ -68,11 +77,11 @@ namespace LeetWars.Core.BLL.Services
             if (page is not null && page.PageSize > 0 && page.PageNumber > 0)
             {
                 challenges = challenges.Skip(page.PageSize * (page.PageNumber - 1))
-                        .Take(page.PageSize);
+                    .Take(page.PageSize);
             }
-            
+
             var filteredChallenges = await challenges.ToListAsync();
-            
+
             //filter runs on the client because filters.TagsIds[] didn't pass to the server and SQL query get error
             if (filters.TagsIds != null)
             {
@@ -85,36 +94,49 @@ namespace LeetWars.Core.BLL.Services
 
             return _mapper.Map<List<ChallengePreviewDto>>(filteredChallenges);
         }
-        
+
         public async Task<ChallengePreviewDto> GetChallengeSuggestionAsync(SuggestionSettingsDto settings)
         {
             var userId = _userIdGetter.CurrentUserId;
-            
+
             var random = new Random(DateTime.Now.Millisecond);
-            
+
             var challenges = _context.Challenges
-                    .Include(challenge => challenge.Tags)
-                    .Include(challenge => challenge.Author)
-                    .Include(challenge => challenge.Level)
-                    .Include(challenge => challenge.Versions)
-                        .ThenInclude(version => version.Language)
-                    .Include(challenge => challenge.Versions)
-                        .ThenInclude(version => version.Solutions)
-                            .ThenInclude(solution => solution.User)
-                    .Where(c => c.Versions.Any(v => v.LanguageId == settings.LanguageId))
-                    .AsQueryable();
-            
+                .Include(challenge => challenge.Tags)
+                .Include(challenge => challenge.Author)
+                .Include(challenge => challenge.Level)
+                .Include(challenge => challenge.Versions)
+                .ThenInclude(version => version.Language)
+                .Include(challenge => challenge.Versions)
+                .ThenInclude(version => version.Solutions)
+                .ThenInclude(solution => solution.User)
+                .Where(c => c.Versions.Any(v => v.LanguageId == settings.LanguageId))
+                .AsQueryable();
+
             challenges = settings.SuggestionType switch
             {
-                SuggestionType.Beta => challenges.Where(challenge => challenge.Versions.Any(version => version.Status == ChallengeStatus.Beta)),
-                SuggestionType.Fundamentals => challenges.Where(challenge => challenge.Level != null && challenge.Level.Name.ToLower().Contains("easy")),
-                SuggestionType.RankUp => challenges.Where(challenge => challenge.Level != null && challenge.Level.Name.ToLower().Contains("medium")),
-                SuggestionType.PracticeAndRepeat => challenges.Where(challenge => challenge.Versions.Any(version => version.Solutions.Any(solution => solution.User != null && solution.User.Uid == userId))),
+                SuggestionType.Beta => challenges.Where(challenge =>
+                    challenge.Versions.Any(version => version.Status == ChallengeStatus.Beta)),
+                SuggestionType.Fundamentals => challenges.Where(challenge =>
+                    challenge.Level != null && challenge.Level.Name.ToLower().Contains("easy")),
+                SuggestionType.RankUp => challenges.Where(challenge =>
+                    challenge.Level != null && challenge.Level.Name.ToLower().Contains("medium")),
+                SuggestionType.PracticeAndRepeat => challenges.Where(challenge => challenge.Versions.Any(version =>
+                    version.Solutions.Any(solution => solution.User != null && solution.User.Uid == userId))),
                 _ => challenges
             };
 
+            using (var generatorProvider = new RNGCryptoServiceProvider())
+            {
+                var data = new byte[4];
+                generatorProvider.GetBytes(data);
+                var randomValue = Math.Abs(BitConverter.ToInt32(data, 0));
 
-            return _mapper.Map<ChallengePreviewDto>(await challenges.Skip(random.Next(0,challenges.Count())).FirstOrDefaultAsync());
+                int randomPosition = (randomValue % challenges.Count());
+                
+                return _mapper.Map<ChallengePreviewDto>(await challenges.Skip(randomPosition)
+                    .FirstOrDefaultAsync());
+            }
         }
     }
 }
