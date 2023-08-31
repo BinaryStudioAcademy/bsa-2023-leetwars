f22ef8798942bf7c8876df0ff3980101d1cd7376
pashamed <pashamedenetes@gmail.com>
2023-08-31 23:40:51 +0300

    resolve reviews

--- frontend/src/app/modules/user/profile-page-main-info/profile-page-main-info.component.ts
+++ frontend/src/app/modules/user/profile-page-main-info/profile-page-main-info.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnChanges } from '@angular/core';
 import { UserFull } from '@shared/models/profile/user-full';
+import * as moment from 'moment/moment';
 
 @Component({
     selector: 'app-profile-page-main-info',
@@ -10,10 +11,6 @@ export class ProfilePageMainInfoComponent implements OnChanges {
     @Input() user: UserFull = <UserFull>{};
 
     //replace it with real user interface
-    ngOnChanges(): void {
-        this.updateView();
-    }
-
     public username? = '';
 
     public languages? = '';
@@ -36,37 +33,19 @@ export class ProfilePageMainInfoComponent implements OnChanges {
 
     public communityLastWeekReputation = 0;
 
-    private updateView() {
-        this.username = this.user.userName;
-        this.languages = this.user.preferredLanguages?.join(', ');
-        this.userRank = this.user.totalScore?.toString();
-        this.communitySolution = this.user.solutions?.length;
-        const lastWeek = this.getLastWeekDates();
+    ngOnChanges(): void {
+        this.updateView();
+    }
 
+    private updateView() {
         if (this.user.solutions) {
             this.communityLastWeekSolution = this.user.solutions?.filter(
                 (solution) =>
-                    new Date(solution.submittedAt!).getTime() < lastWeek[1].getTime() &&
-                    new Date(solution.submittedAt!).getTime() > lastWeek[0].getTime(),
+                    new Date(solution.submittedAt!).getTime() >
+                        new Date(moment().subtract(1, 'weeks').startOf('isoWeek').format('YYYY-MM-DD')).getTime() &&
+                    new Date(solution.submittedAt!).getTime() <
+                        new Date(moment().subtract(1, 'weeks').endOf('isoWeek').format('YYYY-MM-DD')).getTime(),
             ).length;
         }
-    }
-
-    private getLastWeekDates() {
-        const now = new Date();
-        const dayOfWeek = now.getDay();
-        const numDay = now.getDate();
-
-        const start = new Date(now);
-
-        start.setDate(numDay - dayOfWeek - 7);
-        start.setHours(0, 0, 0, 0);
-
-        const end = new Date(now);
-
-        end.setDate(numDay + (7 - dayOfWeek));
-        end.setHours(0, 0, 0, 0);
-
-        return [start, end];
     }
 }
