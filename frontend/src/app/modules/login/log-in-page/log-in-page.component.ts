d6308ce68ee4b278757c10fefff27fa98797042a
Illusion4 <vladyslavlintur@gmail.com>
2023-09-01 11:26:17 +0300

    return mail check

--- frontend/src/app/modules/login/log-in-page/log-in-page.component.ts
+++ frontend/src/app/modules/login/log-in-page/log-in-page.component.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 import { AuthService } from '@core/services/auth.service';
 import { ToastrNotificationsService } from '@core/services/toastr-notifications.service';
 import { UserService } from '@core/services/user.service';
+import { switchMap } from 'rxjs';
 
 @Component({
     selector: 'app-log-in-page',
@@ -39,14 +40,26 @@ export class LogInPageComponent implements OnInit {
     }
 
     public signIn() {
-        this.authService.login(this.logInForm.value.email!, this.logInForm.value.password!).subscribe(
-            () => {
-                this.router.navigateByUrl('/main');
-            },
-            (error) => {
-                this.toastrNotification.showError('Something went wrong');
-                console.error('Error :', error);
-            },
-        );
+        this.userService
+            .checkEmail(this.logInForm.value.email!)
+            .pipe(
+                switchMap((result) => {
+                    if (!result) {
+                        this.isExistingEmail = false;
+                        this.logInForm.markAsUntouched();
+                    }
+
+                    return this.authService.login(this.logInForm.value.email!, this.logInForm.value.password!);
+                }),
+            )
+            .subscribe(
+                () => {
+                    this.router.navigateByUrl('/main');
+                },
+                (error) => {
+                    this.toastrNotification.showError('Something went wrong');
+                    console.error('Error :', error);
+                },
+            );
     }
 }
