6ff39f4e2558c4e5489b41970c7c41fe2f8b1459
SashaBondarchuk <o.bondarchuk9962@gmail.com>
2023-09-03 13:14:47 +0300

    removed login password validation

--- frontend/src/app/modules/login/log-in-page/log-in-page.component.ts
+++ frontend/src/app/modules/login/log-in-page/log-in-page.component.ts
@@ -4,8 +4,6 @@ import { Router } from '@angular/router';
 import { AuthService } from '@core/services/auth.service';
 import { ToastrNotificationsService } from '@core/services/toastr-notifications.service';
 import { UserService } from '@core/services/user.service';
-import { passwordMaxLength, passwordMinLength } from '@shared/utils/validation/form-control-validator-options';
-import { passwordPattern } from '@shared/utils/validation/regex-patterns';
 import { getErrorMessage } from '@shared/utils/validation/validation-helper';
 import { switchMap } from 'rxjs';
 
@@ -17,12 +15,7 @@ import { switchMap } from 'rxjs';
 export class LogInPageComponent implements OnInit {
     logInForm = new FormGroup({
         email: new FormControl('', [Validators.required, Validators.email]),
-        password: new FormControl('', [
-            Validators.required,
-            Validators.minLength(passwordMinLength),
-            Validators.maxLength(passwordMaxLength),
-            Validators.pattern(passwordPattern),
-        ]),
+        password: new FormControl('', [Validators.required]),
     });
 
     isExistingEmail = true;
