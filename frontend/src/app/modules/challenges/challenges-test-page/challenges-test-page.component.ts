1626e884f2cbd906814686c8f2bf81905f2f9e87
Stanislav-Ostr <stanislav.ostr@gmail.com>
2023-09-06 18:38:20 +0300

    Refactored challenge test page component

--- frontend/src/app/modules/challenges/challenges-test-page/challenges-test-page.component.ts
+++ frontend/src/app/modules/challenges/challenges-test-page/challenges-test-page.component.ts
@@ -1,42 +1,136 @@
-import { ChangeDetectorRef, Component } from '@angular/core';
-import { DropdownItem } from '@shared/models/dropdown-item';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { fillFormInputs } from '@modules/challenges/challenge-creation/challenge-creation.utils';
+import { NavigationTabType } from '@shared/enums/navigation-tab-type';
+import { NavigationTab } from '@shared/models/navigation-tab';
+
+import { tabs } from './challenges-test-page.utils';
 
 @Component({
     selector: 'app-challenges-test-page',
     templateUrl: './challenges-test-page.component.html',
     styleUrls: ['./challenges-test-page.component.sass'],
 })
-export class ChallengesTestPageComponent {
-    public languagesList: DropdownItem[] = [{ content: 'JavaScript', iconName: 'node-js' }, { content: 'C#' }];
+export class ChallengesTestPageComponent implements OnInit, OnChanges {
+    @Input() editorLanguage: string;
 
-    public languageVersionsList: DropdownItem[] = [{ content: 'Node v18.x' }, { content: 'Node v17.x' }];
+    @Input() testCases: string;
 
-    public tabs = [
-        { title: 'Test Cases', active: true },
-        { title: 'Example Test Cases', content: 'Example Test Cases content' },
-        { title: 'Help', content: 'Help infomation' },
-    ];
+    @Input() exampleTestCases: string;
 
-    public selectedLanguage: string;
+    @Input() checkValidation = false;
+
+    @Output() testCasesChange = new EventEmitter<string>();
+
+    @Output() exampleTestCasesChange = new EventEmitter<string>();
+
+    @Output() validationChange = new EventEmitter<boolean>();
+
+    public editorContent: string;
+
+    public tabs = tabs;
+
+    public currentTab = tabs[0];
 
     public isMaximized: boolean = false;
 
-    private languageNameMapping: { [key: string]: string } = {
-        'C#': 'csharp',
-    };
+    public inputForm = new FormGroup({
+        testCases: new FormControl('', [
+            Validators.required,
+        ]),
+        exampleTestCases: new FormControl('', [
+            Validators.required,
+        ]),
+    });
 
-    constructor(private cdr: ChangeDetectorRef) {}
+    ngOnInit(): void {
+        this.editorContent = this.testCases;
+    }
+
+    public onCodeChange(code: string) {
+        this.editorContent = code;
+
+        switch (this.currentTab.type) {
+            case NavigationTabType.TestCases:
+                this.testCases = code;
+                this.testCasesChange.emit(code);
+
+                this.inputForm.controls.testCases.setValue(code);
+                this.validationChange.emit(this.inputForm.valid);
+
+                break;
+            case NavigationTabType.ExampleTestCases:
+                this.exampleTestCases = code;
+                this.exampleTestCasesChange.emit(code);
+
+                this.inputForm.controls.exampleTestCases.setValue(code);
+                this.validationChange.emit(this.inputForm.valid);
+
+                break;
+            default:
+                break;
+        }
+    }
+
+    switchTab(tab: NavigationTab): void {
+        this.currentTab = tab;
+        this.updateEditorContent();
+    }
+
+    public updateEditorContent() {
+        switch (this.currentTab.type) {
+            case NavigationTabType.TestCases:
+                this.editorContent = this.testCases;
+                break;
+            case NavigationTabType.ExampleTestCases:
+                this.editorContent = this.exampleTestCases;
+                break;
+            default:
+                break;
+        }
+    }
+
+    ngOnChanges(changes: SimpleChanges): void {
+        if (changes['checkValidation']) {
+            if (this.checkValidation) {
+                this.inputForm.markAllAsTouched();
+            }
+        }
+        fillFormInputs(this.inputForm, changes);
+        this.updateEditorContent();
+    }
+
+    getTabClass(tab: NavigationTab) {
+        let inputError = false;
+
+        switch (tab.type) {
+            case NavigationTabType.TestCases:
+                inputError = this.inputForm.controls.testCases.touched && this.inputForm.controls.testCases.invalid;
+                break;
+            case NavigationTabType.ExampleTestCases:
+                inputError = this.inputForm.controls.exampleTestCases.touched && this.inputForm.controls.exampleTestCases.invalid;
+                break;
+            default:
+                break;
+        }
+
+        return inputError ? 'input-error' : '';
+    }
+
+    editorFocusOut() {
+        switch (this.currentTab.type) {
+            case NavigationTabType.TestCases:
+                this.inputForm.controls.testCases.markAsTouched();
+                break;
+            case NavigationTabType.ExampleTestCases:
+                this.inputForm.controls.exampleTestCases.markAsTouched();
+                break;
+            default:
+                break;
+        }
+    }
 
     toggleMaximize() {
         this.isMaximized = !this.isMaximized;
-    }
-
-    onSelectedItemChanged(selectedItem: DropdownItem) {
-        this.selectedLanguage = this.mapLanguageName(selectedItem.content);
-        this.cdr.detectChanges();
-    }
-
-    private mapLanguageName(language: string): string {
-        return this.languageNameMapping[language] || language.toLowerCase();
     }
 }
