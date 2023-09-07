dafec40bb2e3150f81c921c4604ea9511d9487e8
SashaBondarchuk <o.bondarchuk9962@gmail.com>
2023-09-07 21:13:30 +0300

    added email sending logic

--- backend/LeetWars.Emailer/Program.cs
+++ backend/LeetWars.Emailer/Program.cs
@@ -1,6 +1,19 @@
+using FluentValidation;
+using LeetWars.Core.Common.DTO.Mail;
+using LeetWars.Emailer.Interfaces;
+using LeetWars.Emailer.Services;
+using LeetWars.Emailer.Validators;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<IMailService, MailService>();
+builder.Services.AddScoped<IValidator<MailDto>, MailDtoValidator>();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapPost("/emailer/sendEmail", (IMailService mailService, MailDto mailDto) =>
+{
+    return mailService.SendWithValidationCheck(mailDto);
+});
 
 app.Run();
---
+++ backend/LeetWars.Emailer/Services/MailService.cs
@@ -0,0 +1,44 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using LeetWars.Core.Common.DTO.Mail;
+using LeetWars.Emailer.Interfaces;
+using SendGrid.Helpers.Mail;
+using SendGrid;
+
+namespace LeetWars.Emailer.Services
+{
+    public class MailService : IMailService
+    {
+        private readonly IValidator<MailDto> _validator;
+        private readonly IConfiguration _configuration;
+        private readonly SendGridClient _sendGridClient;
+
+        public MailService(IConfiguration configuration, IValidator<MailDto> validator)
+        {
+            _configuration = configuration;
+            _validator = validator;
+            var apiKey = Environment.GetEnvironmentVariable("SENDGRID_API_KEY");
+            _sendGridClient = new SendGridClient(apiKey);
+        }
+
+        private async Task SendEmailAsync(MailDto mailDto)
+        {
+            var senderEmail = new EmailAddress(_configuration["Email"], _configuration["EmailSenderName"]);
+            var receiverEmail = new EmailAddress(mailDto.ReceiverEmail);
+            var message = MailHelper.CreateSingleEmail(senderEmail, receiverEmail, mailDto.Subject, mailDto.Content, mailDto.Content);
+            await _sendGridClient.SendEmailAsync(message);
+        }
+
+        public async Task<IResult> SendWithValidationCheck(MailDto mailDto)
+        {
+            ValidationResult validationResult = await _validator.ValidateAsync(mailDto);
+            if (!validationResult.IsValid)
+            {
+                return Results.ValidationProblem(validationResult.ToDictionary());
+            }
+
+            await SendEmailAsync(mailDto);
+            return Results.Created($"/{mailDto.ReceiverEmail}", mailDto);
+        }
+    }
+}
