2c89e9692953d1f75a0afecea72ef379e19ffcd6
pospyra <pos.pyra@yandex.ru>
2023-09-08 12:21:24 +0300

    added [options] to code-editor

--- frontend/src/app/shared/components/code-editor/code-editor.component.ts
+++ frontend/src/app/shared/components/code-editor/code-editor.component.ts
@@ -1,35 +1,30 @@
-import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
-
-interface EditorOptions {
-    language: string;
-    theme: string;
-    minimap: {
-        enabled: boolean;
-    };
-    automaticLayout: boolean;
-    useShadows: boolean;
-    wordWrap: string;
-    lineNumbers: string;
-}
+import { AfterViewInit, Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
+import { EditorOptions } from '@shared/models/options/editor-options';
 
 @Component({
     selector: 'app-code-editor[language]',
     templateUrl: './code-editor.component.html',
     styleUrls: ['./code-editor.component.sass'],
 })
-export class CodeEditorComponent implements OnChanges, OnInit {
+export class CodeEditorComponent implements OnChanges, AfterViewInit, OnInit {
     languageNameMap: Map<string, string> = new Map<string, string>([['C#', 'csharp']]);
 
     @Input() language: string;
 
-    @Input() initSolution: string = '';
+    @Input() initText: string = '';
 
     @Output() codeChanged = new EventEmitter<string>();
 
+    @Input() options: EditorOptions;
+
     editorOptions: EditorOptions;
 
+    ngAfterViewInit(): void {
+        this.updateEditorOptions();
+    }
+
     public onCodeChange(model: string) {
-        this.initSolution = model;
+        this.initText = model;
         this.codeChanged.emit(model);
     }
 
@@ -43,19 +38,15 @@ export class CodeEditorComponent implements OnChanges, OnInit {
         this.updateEditorOptions();
     }
 
-    private mapLanguageName(language: string): string {
-        return this.languageNameMap.get(language) || language.toLowerCase();
-    }
-
     private updateEditorOptions(): void {
         this.editorOptions = {
-            theme: 'custom-theme',
-            language: this.mapLanguageName(this.language),
-            minimap: { enabled: false },
-            automaticLayout: true,
-            useShadows: false,
-            wordWrap: 'on',
-            lineNumbers: 'on',
+            theme: this.options?.theme,
+            language: this.options?.language,
+            minimap: { enabled: this.options?.minimap?.enabled },
+            automaticLayout: this.options?.automaticLayout,
+            useShadows: this.options?.useShadows,
+            wordWrap: this.options?.wordWrap,
+            lineNumbers: this.options?.lineNumbers,
         };
     }
 }
