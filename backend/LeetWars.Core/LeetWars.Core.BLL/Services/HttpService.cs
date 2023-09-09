336ff8d80d7c33a4c1219564424f3f7eefd8a539
SashaBondarchuk <o.bondarchuk9962@gmail.com>
2023-09-09 13:04:51 +0300

    fix code smells

--- backend/LeetWars.Core/LeetWars.Core.BLL/Services/HttpService.cs
+++ backend/LeetWars.Core/LeetWars.Core.BLL/Services/HttpService.cs
@@ -6,15 +6,30 @@ namespace LeetWars.Core.BLL.Services
     public class HttpService : IHttpService
     {
         private readonly HttpClient _httpClient;
+        private bool disposedValue;
 
         public HttpService()
         {
             _httpClient = new HttpClient();
         }
 
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    _httpClient.Dispose();
+                }
+
+                disposedValue = true;
+            }
+        }
+
         public void Dispose()
         {
-            _httpClient.Dispose();
+            Dispose(true);
+            GC.SuppressFinalize(this);
         }
 
         public async Task<HttpResponseMessage> PostRequestAsync(string uri, string content)
