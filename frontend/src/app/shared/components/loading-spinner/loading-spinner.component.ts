1506dba6445270adda7d46e67076babe0f2a9b7c
pospyra <pos.pyra@yandex.ru>
2023-09-11 15:46:01 +0300

    styles moved to ts file

--- frontend/src/app/shared/components/loading-spinner/loading-spinner.component.ts
+++ frontend/src/app/shared/components/loading-spinner/loading-spinner.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { SpinnerService } from '@core/services/spinner.service';
 
 @Component({
     selector: 'app-loading-spinner',
     templateUrl: './loading-spinner.component.html',
     styleUrls: ['./loading-spinner.component.sass'],
-
 })
 export class LoadingSpinnerComponent {
-    constructor(public spinner: SpinnerService) {}
+    constructor(public spinnerService: SpinnerService) {}
+
+    @Input() overlay: boolean;
+
+    @Input() size = '20px';
+
+    @Input() top = '50%';
+
+    @Input() left = '50%';
+
+    @Input() position = 'fixed';
+
+    @Input() margin = 'auto';
 }
