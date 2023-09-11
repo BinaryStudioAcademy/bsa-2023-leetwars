8e1c28a8189694ef3f898aab19acc8d7c4e5b8da
pospyra <pos.pyra@yandex.ru>
2023-09-11 12:20:23 +0300

    added styles for loading spinner

--- frontend/src/app/shared/components/loading-spinner/loading-spinner.component.ts
+++ frontend/src/app/shared/components/loading-spinner/loading-spinner.component.ts
@@ -1,22 +1,12 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { SpinnerService } from '@core/services/spinner.service';
 
 @Component({
     selector: 'app-loading-spinner',
     templateUrl: './loading-spinner.component.html',
+    styleUrls: ['./loading-spinner.component.sass'],
+
 })
 export class LoadingSpinnerComponent {
-    constructor(public spinnerService: SpinnerService) {}
-
-    @Input() overlay: boolean;
-
-    @Input() size = '20px';
-
-    @Input() top = '30%';
-
-    @Input() left = '49%';
-
-    @Input() position = 'absolute';
-
-    @Input() margin = '100px auto';
+    constructor(public spinner: SpinnerService) {}
 }
