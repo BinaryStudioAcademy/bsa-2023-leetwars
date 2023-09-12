43594c68d5baa4fc77a684b0c508681808525e24
Bohdan <bodichka322@gmail.com>
2023-09-12 20:05:19 +0300

    Add validator

---
+++ backend/LeetWars.Core/LeetWars.Core.WebAPI/Validators/User/PageSettingsDTOValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using LeetWars.Core.Common.DTO.Filters;
+using LeetWars.Core.Common.DTO.User;
+
+namespace LeetWars.Core.WebAPI.Validators.User
+{
+    public class PageSettingsDTOValidator : AbstractValidator<PageSettingsDto>
+    {
+        public PageSettingsDTOValidator()
+        {
+            RuleFor(p => p.PageNumber).GreaterThan(0).WithMessage("Page number must be greated than 0");
+
+            RuleFor(p => p.PageSize).GreaterThan(0).WithMessage("Page size must be greated than 0");
+
+        }
+    }
+}
