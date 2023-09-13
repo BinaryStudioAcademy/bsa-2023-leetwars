98970e67ac80862f5a790b08518d92e393db9d9b
Limbo2332 <vorobiovalosha@gmail.com>
2023-09-13 19:32:06 +0300

    removed superfluous data

--- backend/LeetWars.CodeAnalyzer/OpenAISettings/OpenAiCompletionSettings.cs
+++ backend/LeetWars.CodeAnalyzer/OpenAISettings/OpenAiCompletionSettings.cs
@@ -6,6 +6,5 @@
         public static double Temperature = 0.5;
         public static double PresencePenalty = 0.1;
         public static double FrequencyPenalty = 0.1;
-        public static string ApiVersion = "2023-03-15-preview";
     }
 }
