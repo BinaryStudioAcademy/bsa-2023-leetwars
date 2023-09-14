1889b18fce643af847ce649f1d0d48c8c285408c
AyTee001 <tiutiunanastasia@gmail.com>
2023-09-14 23:05:40 +0300

    Add static consumer setup helper

---
+++ backend/LeetWars.RabbitMQ/ConsumerSetupHelper.cs
@@ -0,0 +1,59 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace LeetWars.RabbitMQ
+{
+    public static class ConsumerSetupHelper
+    {
+        public static void ListenSetup(EventHandler<BasicDeliverEventArgs> messageReceivedHandler, IModel channel, ConsumerSettings settings)
+        {
+            SetupChannel(channel, settings);
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            consumer.Received += messageReceivedHandler;
+
+            if (settings.SequentialFetch)
+            {
+                channel.BasicQos(0, 1, false);
+            }
+            channel.BasicConsume(settings.QueueName, settings.AutoAcknowledge, consumer);
+        }
+
+        public static void ListenAsyncSetup(AsyncEventHandler<BasicDeliverEventArgs> messageReceivedHandler, IModel channel, ConsumerSettings settings)
+        {
+            SetupChannel(channel, settings);
+
+            var consumer = new AsyncEventingBasicConsumer(channel);
+
+            consumer.Received += messageReceivedHandler;
+
+            if (settings.SequentialFetch)
+            {
+                channel.BasicQos(0, 1, false);
+            }
+
+            channel.BasicConsume(settings.QueueName, settings.AutoAcknowledge, consumer);
+        }
+
+        public static void SetAcknowledgeSetup(ulong deliveryTag, bool processed, IModel channel)
+        {
+            if (processed)
+            {
+                channel.BasicAck(deliveryTag, false);
+            }
+            else
+            {
+                channel.BasicNack(deliveryTag, false, true);
+            }
+        }
+
+        private static void SetupChannel(IModel channel, ConsumerSettings settings)
+        {
+            channel.ExchangeDeclare(settings.ExchangeName, settings.ExchangeType);
+            channel.QueueDeclare(settings.QueueName, true, false, false);
+            channel.QueueBind(settings.QueueName, settings.ExchangeName, settings.RoutingKey);
+        }
+
+    }
+}
--- backend/LeetWars.RabbitMQ/Services/NotifierCodeConsumerService.cs
+++ backend/LeetWars.RabbitMQ/Services/NotifierCodeConsumerService.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using RabbitMQ.Client;
+﻿using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 
 namespace LeetWars.RabbitMQ.Services;
@@ -23,55 +18,17 @@ public class NotifierCodeConsumerService : IConsumerService
 
     public void Listen(EventHandler<BasicDeliverEventArgs> messageReceivedHandler)
     {
-        SetupChannel();
-
-        var consumer = new EventingBasicConsumer(_channel);
-
-        consumer.Received += messageReceivedHandler;
-
-        if (_settings.SequentialFetch)
-        {
-            _channel.BasicQos(0, 1, false);
-        }
-
-        _channel.BasicConsume(_settings.QueueName, _settings.AutoAcknowledge, consumer);
+        ConsumerSetupHelper.ListenSetup(messageReceivedHandler, _channel, _settings);
     }
 
     public void ListenAsync(AsyncEventHandler<BasicDeliverEventArgs> messageReceivedHandler)
     {
-        SetupChannel();
-
-        var consumer = new AsyncEventingBasicConsumer(_channel);
-
-        consumer.Received += messageReceivedHandler;
-
-        if (_settings.SequentialFetch)
-        {
-            _channel.BasicQos(0, 1, false);
-        }
-
-        _channel.BasicConsume(_settings.QueueName, _settings.AutoAcknowledge, consumer);
+        ConsumerSetupHelper.ListenAsyncSetup(messageReceivedHandler, _channel, _settings);
     }
 
     public void SetAcknowledge(ulong deliveryTag, bool processed)
     {
-        if (processed)
-        {
-            _channel.BasicAck(deliveryTag, false);
-        }
-        else
-        {
-            _channel.BasicNack(deliveryTag, false, true);
-        }
-    }
-
-    private void SetupChannel()
-    {
-        _channel.ExchangeDeclare(_settings.ExchangeName, _settings.ExchangeType);
-
-        _channel.QueueDeclare(_settings.QueueName, true, false, false);
-
-        _channel.QueueBind(_settings.QueueName, _settings.ExchangeName, _settings.RoutingKey);
+        ConsumerSetupHelper.SetAcknowledgeSetup(deliveryTag, processed, _channel);
     }
 
     protected virtual void Dispose(bool disposing)
