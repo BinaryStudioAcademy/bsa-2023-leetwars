4f8fb242c7e891ccf62d68213dd36cd3f59bafdc
Bohdan <bodichka322@gmail.com>
2023-09-15 23:29:38 +0300

    Add debounce for input

--- frontend/src/app/modules/main/filtering-section/filtering-section.component.ts
+++ frontend/src/app/modules/main/filtering-section/filtering-section.component.ts
@@ -9,13 +9,14 @@ import {
     SORTING_PROPERTIES,
     STATUS_NAMES_MAP,
 } from '@modules/main/filtering-section/filtering-section.utils';
+import { SecondsConstants } from '@shared/constants/second-constants';
 import { IChallengeFilter } from '@shared/models/challenge/challenge-filter';
 import { IChallengePreview } from '@shared/models/challenge/challenge-preview';
 import { ISortedModel } from '@shared/models/challenge/sorted-model';
 import { ILanguage } from '@shared/models/language/language';
 import { IPageSettings } from '@shared/models/page-settings';
 import { ITag } from '@shared/models/tag/tag';
-import { takeUntil } from 'rxjs';
+import { Subject, debounceTime, takeUntil } from 'rxjs';
 
 @Component({
     selector: 'app-filtering-section',
@@ -59,6 +60,8 @@ export class FilteringSectionComponent extends BaseComponent implements OnInit {
 
     private statuses = STATUS_NAMES_MAP;
 
+    private searchSubject = new Subject<string>();
+
     constructor(
         private challengeService: ChallengeService,
         private languageService: LanguageService,
@@ -75,6 +78,13 @@ export class FilteringSectionComponent extends BaseComponent implements OnInit {
 
         this.statusesNames = this.statuses.map((item) => item.name);
         this.progressesNames = this.progresses.map((item) => item.name);
+
+        this.searchSubject
+            .pipe(debounceTime(SecondsConstants.Delay))
+            .subscribe((value) => {
+            this.filter.title = value.trim();
+            this.resetChallengesData();
+        });
     }
 
     public onScroll() {
@@ -86,8 +96,7 @@ export class FilteringSectionComponent extends BaseComponent implements OnInit {
     }
 
     public onSearchTextChange(value: string) {
-        this.filter.title = value.trim();
-        this.resetChallengesData();
+       this.searchSubject.next(value);
     }
 
     public onSortChange(value: string | string[]) {
