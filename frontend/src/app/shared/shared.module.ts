1858d83ba78e8c786c36622dde527fdb11c8fc9d
Limbo2332 <vorobiovalosha@gmail.com>
2023-09-20 23:53:07 +0300

    fixed changes, improved validation messages

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -23,7 +23,6 @@ import { HeaderComponent } from './components/header/header.component';
 import { LoadingSpinnerComponent } from './components/loading-spinner/loading-spinner.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { NotificationsComponent } from './components/notifications/notifications.component';
-import { PieChartComponent } from './components/pie-chart/pie-chart.component';
 import { ProgressBarComponent } from './components/progress-bar/progress-bar.component';
 
 @NgModule({
@@ -50,6 +49,7 @@ import { ProgressBarComponent } from './components/progress-bar/progress-bar.com
         CustomInputComponent,
         CodeEditorComponent,
         ProgressBarComponent,
+        NotificationsComponent,
         HeaderComponent,
     ],
     exports: [
