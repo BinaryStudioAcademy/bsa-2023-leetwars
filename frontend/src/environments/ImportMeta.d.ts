65a1a6661a76ff605f46b385dd4868a118996b44
Limbo2332 <vorobiovalosha@gmail.com>
2023-09-22 18:44:46 +0300

    fixed B24 - folders in projects

--- frontend/src/environments/ImportMeta.d.ts
+++ frontend/src/environments/ImportMeta.d.ts
@@ -1,5 +1,3 @@
-import { ImportMetaEnv } from './env';
-
 interface ImportMeta {
     readonly env: ImportMetaEnv;
 }
--- frontend/src/environments/env.d.ts
+++ frontend/src/environments/env.d.ts
@@ -1,4 +1,4 @@
-export interface ImportMetaEnv {
+interface ImportMetaEnv {
     /**
    * Built-in environment variable.
    * @see Docs https://github.com/chihab/ngx-env#ng_app_env.
