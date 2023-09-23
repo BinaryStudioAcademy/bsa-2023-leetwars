73029880b5b780ef40b437330b5b2a95d9803712
pospyra <pos.pyra@yandex.ru>
2023-09-23 14:56:11 +0300

    changed the string to enum suggestionType

--- frontend/src/app/modules/main/suggested-challenge/suggested-challenge.component.ts
+++ frontend/src/app/modules/main/suggested-challenge/suggested-challenge.component.ts
@@ -21,6 +21,8 @@ import { takeUntil } from 'rxjs';
     styleUrls: ['./suggested-challenge.component.sass'],
 })
 export class SuggestedChallengeComponent extends BaseComponent implements OnInit {
+    suggestionType = SuggestionType;
+
     challenge?: IChallengePreview;
 
     languagesNames: string[] = [];
