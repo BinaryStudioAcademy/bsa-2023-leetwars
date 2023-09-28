546dd14d5ee897646e4b65b144ddae1d052c33cd
SashaBondarchuk <o.bondarchuk9962@gmail.com>
2023-09-28 15:19:13 +0300

    code cleanup

--- backend/LeetWars.Core/LeetWars.Core.BLL/Interfaces/IChallengeService.cs
+++ backend/LeetWars.Core/LeetWars.Core.BLL/Interfaces/IChallengeService.cs
@@ -5,7 +5,6 @@ using LeetWars.Core.Common.DTO.CodeFight;
 using LeetWars.Core.Common.DTO.CodeRunRequest;
 using LeetWars.Core.Common.DTO.Filters;
 using LeetWars.Core.Common.DTO.SortingModel;
-using LeetWars.Core.DAL.Entities;
 
 namespace LeetWars.Core.BLL.Interfaces
 {
--- backend/LeetWars.Core/LeetWars.Core.WebAPI/Validators/Challenge/NewChallengeDtoValidator.cs
+++ backend/LeetWars.Core/LeetWars.Core.WebAPI/Validators/Challenge/NewChallengeDtoValidator.cs
@@ -7,27 +7,27 @@ namespace LeetWars.Core.WebAPI.Validators.Challenge
     {
         public NewChallengeDtoValidator()
         {
-            RuleFor(dto => dto.Title).NotEmpty().WithMessage("Title is required");
-            RuleFor(dto => dto.Instructions).NotEmpty().WithMessage("Instructions are required");
-            RuleFor(dto => dto.LevelId).NotEmpty().WithMessage("LevelId is required");
-            RuleFor(dto => dto.Category).NotEmpty().WithMessage("Category is required");
-            RuleFor(dto => dto.Tags).NotEmpty().WithMessage("Tags are required");
-
             RuleForEach(dto => dto.Versions)
                 .ChildRules(challengeVersion =>
                 {
-                    challengeVersion.RuleFor(cv => cv.InitialSolution)
-                        .NotEmpty().WithMessage("InitialSolution is required");
+                    challengeVersion.RuleFor(cv => cv.ExampleTestCases)
+                        .NotEmpty().WithMessage("ExampleTestCases are required!");
 
-                    challengeVersion.RuleFor(cv => cv.CompleteSolution)
-                        .NotEmpty().WithMessage("CompleteSolution is required");
+                    challengeVersion.RuleFor(cv => cv.InitialSolution)
+                        .NotEmpty().WithMessage("InitialSolution is required!");
 
                     challengeVersion.RuleFor(cv => cv.TestCases)
-                        .NotEmpty().WithMessage("TestCases are required");
+                        .NotEmpty().WithMessage("TestCases are required!");
 
-                    challengeVersion.RuleFor(cv => cv.ExampleTestCases)
-                        .NotEmpty().WithMessage("ExampleTestCases are required");
+                    challengeVersion.RuleFor(cv => cv.CompleteSolution)
+                        .NotEmpty().WithMessage("CompleteSolution is required!");
                 });
+
+            RuleFor(dto => dto.LevelId).NotEmpty().WithMessage("LevelId is required!");
+            RuleFor(dto => dto.Category).NotEmpty().WithMessage("Category is required!");
+            RuleFor(dto => dto.Tags).NotEmpty().WithMessage("Tags are required!");
+            RuleFor(dto => dto.Title).NotEmpty().WithMessage("Title is required!");
+            RuleFor(dto => dto.Instructions).NotEmpty().WithMessage("Instructions are required!");
         }
     }
 }
