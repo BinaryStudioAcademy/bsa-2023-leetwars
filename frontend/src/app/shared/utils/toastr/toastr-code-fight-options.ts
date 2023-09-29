9762c2c9093f9462a3241cd62b6f946ceb4994d1
Limbo2332 <vorobiovalosha@gmail.com>
2023-09-29 18:51:44 +0300

    fixed modals for codefight winner and loser

---
+++ frontend/src/app/shared/utils/toastr/toastr-code-fight-options.ts
@@ -0,0 +1,6 @@
+import { IndividualConfig } from 'ngx-toastr';
+
+export const toastrCodeFightOptions: Partial<IndividualConfig> = {
+    positionClass: 'toast-bottom-right',
+    progressBar: true,
+};
